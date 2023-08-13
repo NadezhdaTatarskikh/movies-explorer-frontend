6b12a96431513245be05709465c2cce0fc917117
NadezhdaTatarskikh <git@github.com:NadezhdaTatarskikh>
2023-08-13 15:37:25 +0300

    ошибки чекбокса/4

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -172,6 +172,7 @@ function App() {
     }
   }, [foundMoviesList, savedMovies, searchKeyword]);
 
+
   // Отслеживаем состояние стэйта чекбокса
   useEffect(() => {
     if (localStorage.getItem('shortSavedMovieCheckbox') === 'true') {
